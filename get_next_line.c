5a11e794cf7c6477462c564355ed669ea234d17c
Derek Montesdeoca <demont93@gmail.com>
2025-03-18 05:33:10 +0100

    Remove buffer frees.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dmontesd <dmontesd@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:07:39 by dmontesd          #+#    #+#             */
-/*   Updated: 2025/03/18 05:24:04 by dmontesd         ###   ########.fr       */
+/*   Updated: 2025/03/18 05:33:09 by dmontesd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,9 +82,9 @@ char	*get_next_line(int fd)
 		if (buf_read_result == BUF_READ_RESULT_DONE)
 			break ;
 		if (buf_read_result == BUF_READ_RESULT_ERROR)
-			return (free(line.data), free(buf.buf), NULL);
+			return (free(line.data), NULL);
 		if (!append_from_buf_to_line(&buf, &line))
-			return (free(line.data), free(buf.buf), NULL);
+			return (free(line.data), NULL);
 	}
 	return (line.data);
 }
