16f519db8132c4ad873f0bd2b46e8bc3989cd7a1
Derek Montesdeoca <demont93@gmail.com>
2025-03-26 14:11:53 +0100

    Added bufffer and fd precondition checks to non-bonus files.

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,15 @@
 /*   By: dmontesd <dmontesd@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:07:39 by dmontesd          #+#    #+#             */
-/*   Updated: 2025/03/20 05:32:35 by dmontesd         ###   ########.fr       */
+/*   Updated: 2025/03/26 14:10:56 by dmontesd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include "get_next_line.h"
+
+#define FD_MAX 1024
 
 bool	append_from_buf_to_line(t_buf *buf, t_dynstr *line)
 {
@@ -76,7 +79,7 @@ char	*get_next_line(int fd)
 	t_dynstr			line;
 	t_buf_read_result	buf_read_result;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd >= FD_MAX || BUFFER_SIZE <= 0 || BUFFER_SIZE > SIZE_MAX)
 		return (free(buf.buf), buf.buf = NULL, NULL);
 	if (!buf_init(&buf, fd))
 		return (NULL);
