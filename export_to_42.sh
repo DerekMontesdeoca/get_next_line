330208878d9c3066e56f878481662e64f84741c9
Derek Montesdeoca <demont93@gmail.com>
2025-03-27 01:30:22 +0100

    Enable --delete option to make rsync "sync" the dir.

--- export_to_42.sh
+++ export_to_42.sh
@@ -4,4 +4,8 @@ DEST_DIR="$1"
 SOURCE_DIR="$(dirname "$(realpath "$0")")"
 
 mkdir -p "$DEST_DIR"
-rsync -azv --delete "$SOURCE_DIR/"*.{h,c} "$DEST_DIR"
+rsync -azv --delete \
+    --files-from=<(find "$SOURCE_DIR" -maxdepth 1 -mindepth 1 \
+        -regextype "awk" -regex '.*\.(h|c)' -exec basename '{}' ';') \
+    "$SOURCE_DIR/" \
+    "$DEST_DIR"
